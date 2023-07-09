091c884162198b599a98a621ce010e02e94ca4b3
fvaldes0109 <fvaldes0109@gmail.com>
2023-07-09 09:47:24 -0400

    Menu to game fade

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class LevelManager : MonoBehaviour
 {
     
+    public Image blackFade;
+
+    void Start() {
+
+        StartCoroutine(FadeFromBlack());
+    }
+
+    IEnumerator FadeFromBlack() {
+        
+        float alpha = 1;
+        while (alpha > 0) {
+            alpha -= Time.deltaTime;
+            if (alpha < 0) alpha = 0;
+            blackFade.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+    }
+
     public void NextLevel() {
 
         
--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MainMenuManager : MonoBehaviour
 {
     
     public GameObject continueButton;
+    public Image blackFade;
 
     void Start() {
 
@@ -20,6 +23,20 @@ public class MainMenuManager : MonoBehaviour
             Persistents.Level = 1;
             Persistents.currentScore = 0;
             Persistents.upgradeLevels = new int[5];
+        }
+
+        StartCoroutine(FadeToBlack());
+
+        // SceneManager.LoadScene(1);
+    }
+
+    IEnumerator FadeToBlack() {
+            
+        float alpha = 0;
+        while (alpha < 1) {
+            alpha += Time.deltaTime * Persistents.fadeSpeed;
+            blackFade.color = new Color(0, 0, 0, alpha);
+            yield return null;
         }
 
         SceneManager.LoadScene(1);
